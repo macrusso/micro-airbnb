0d077fb271409c1eca54d64d00803405029837fd
rusekmac <mac.rusek1@gmail.com>
2017-10-17 14:39:18 +0100

    Swaping to Materialize, navbar, footbar and forms so far.

---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(e){
+
+    $(".button-collapse").sideNav();
+
+    $('#search-icon').click(function(e){
+        $('#search-icon').hide();
+        $('#search-div').fadeIn();
+        $('#search-txt').focus();
+    });
+
+    $('#search-txt').blur(function(e) {
+        $('#search-div').hide();
+        $('#search-icon').fadeIn();
+    });
+
+});
